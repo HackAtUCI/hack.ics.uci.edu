57a39d4787f9a4514918e5e28bffeae7c6bf06db
Alexander Liu <a@alexanderliu.com>
2023-06-05 03:06:38 -0700

    feat: sanity studio hack blue theme

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'sanity'
+import {theme} from 'https://themer.sanity.build/api/hues?primary=1c3385;800'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
@@ -6,6 +7,7 @@ import {schemaTypes} from './schemas'
 export default defineConfig({
   name: 'default',
   title: 'hack.ics.uci.edu',
+  theme,
 
   projectId: 'ue554f0d',
   dataset: 'production',
