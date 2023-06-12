1781059c58816a4ddcf61c12aff25a0ded78411d
Alexander Liu <a@alexanderliu.com>
2023-06-12 04:44:24 -0700

    refactor: remove default schemas

--- apps/studio/schemas/index.ts
+++ apps/studio/schemas/index.ts
@@ -1,8 +1,4 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 import socials from './socials'
 import icon from './icon'
 
-export const schemaTypes = [post, author, category, blockContent, socials, icon]
+export const schemaTypes = [socials, icon]
